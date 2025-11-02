5c56288340fe616569f659a90cded64d6d05912d
Jenny <j2130970@gmail.com>
2025-11-02 18:08:48 +0800

    style: add UI for RoundStartView

--- src/views/RoundStartView.vue
+++ src/views/RoundStartView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import questionIcon from '@/assets/images/roundStart/questionIcon.png'
 import { supabase } from '@/lib/supabaseClient'
 import { useGlobalStore } from '@/stores/global'
 import { useMatchStore } from '@/stores/match'
@@ -27,12 +28,18 @@ const matchStore = useMatchStore()
 
 const route = useRoute()
 const matchId = route.params.matchId
+const { quizList } = storeToRefs(quizStore)
 
 const { quizSetId } = matchStore.matchData
 const { myRoundList, phantomRoundList } = storeToRefs(roundStore)
 const { userInfo, opponentInfo } = storeToRefs(userStore)
 
 const currentRound = roundStore.myRoundList.length
+const totalRounds = 5
+
+const currentQuiz = quizList.value[currentRound - 1]
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const currentQuizImage = supabaseUrl + currentQuiz?.imageUrl
 
 async function createNewRound() {
   try {
@@ -241,25 +248,168 @@ onMounted(async () => {
     safeReplace(`/`)
   }
 })
+
+const repeatCount = 4
+const space = ' '.repeat(5)
+const text = `QUESTION ${myRoundList.value.length}${space}`.repeat(repeatCount)
+const chars = text.split('')
+const step = 360 / chars.length
+const radius = 'min(50vh, 50vw)'
 </script>
 
 <template>
-  <div class="round-view">
-    <h1>Round {{ myRoundList.length }}</h1>
+  <div class="view-wrapper">
+    <div class="circle-text bungee-regular-36">
+      <span
+        v-for="(char, index) in chars"
+        :key="index"
+        class="char"
+        :style="{
+          transform: `rotate(${index * step}deg) translate(${radius}) rotate(90deg)`,
+        }"
+      >
+        {{ char }}
+      </span>
+    </div>
+
+    <div class="round-card">
+      <p class="bungee-regular-92">Round {{ currentRound }}</p>
+    </div>
+
+    <div class="question-card">
+      <div class="question-head">
+        <img :src="questionIcon" class="head-img" />
+        <p class="bungee-regular-36">QUESTION</p>
+      </div>
+
+      <div class="question-content">
+        <div class="img-container">
+          <img :src="currentQuizImage" />
+        </div>
+
+        <div class="dot-container" :style="{ '--current-index': currentRound }">
+          <div
+            v-for="index in totalRounds"
+            :key="index"
+            class="dot"
+            :class="{ active: index + 1 === currentRound }"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
-.round-view {
+.view-wrapper {
   min-height: 100vh;
-  min-width: 100vw;
-  border: 1px solid #ccc;
-}
-.users-box {
+  background-image:
+    url('@/assets/images/roundStart/roundStartBackground.png'),
+    linear-gradient(to bottom, var(--color-teal-500), var(--color-teal-400));
+  background-size:
+    auto 100%,
+    cover;
+  background-position: left, center;
+  background-repeat: repeat, no-repeat;
+
   display: flex;
-  gap: 24px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  position: relative;
 }
-.user-box {
-  border: 1px solid red;
+
+.circle-text {
+  position: absolute;
+
+  color: var(--color-neutral-50);
+}
+
+.char {
+  position: absolute;
+
+  transform-origin: 0 0;
+}
+
+.round-card {
+  width: 466px;
+  height: 138px;
+  background-color: var(--color-neutral-50);
+  border: 4px solid var(--color-blue-500);
+  transform: rotate(-5deg);
+  box-shadow: var(--shadow-5);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.question-card {
+  width: 420px;
+  padding: 35px 0;
+  background-color: var(--color-neutral-50);
+  border-radius: 32px 20px;
+  box-shadow: var(--shadow-6);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+}
+
+.question-head {
+  display: flex;
+  justify-content: center;
+  gap: 3px;
+}
+
+.head-img {
+  width: 48px;
+  height: 48px;
+}
+
+.question-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 15px;
+}
+
+.img-container {
+  width: 340px;
+  height: 340px;
+  padding: 10px;
+  border: 2px solid var(--color-blue-500);
+  border-radius: 8px;
+}
+
+.img-container img {
+  width: 100%;
+  border-radius: 8px;
+}
+
+.dot-container {
+  display: flex;
+  justify-content: center;
+  gap: 8px;
+}
+
+.dot-container {
+  display: flex;
+  justify-content: center;
+  gap: 8px;
+}
+
+.dot {
+  width: 16px;
+  height: 16px;
+  border-radius: 16px;
+  background-color: var(--color-neutral-1000);
+}
+
+.dot.active {
+  background-color: var(--color-red-400);
+  border: 1px solid var(--color-neutral-900);
 }
 </style>
